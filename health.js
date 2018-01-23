a91bc93c1624af5e22a8d9ffacc0a9c070cb678d
tusharjain <tushar.jain@affle.com>
2018-01-23 23:32:59 +0530

    logs added

--- health.js
+++ health.js
@@ -2,9 +2,15 @@ let express = require('express'),
 app = new express(),
 path = require('path');
 
-app.listen(3000);
+
 
 app.use(express.static('./'));
+
+app.get('/',function(req,res){
+    console.log("request get /---------------");
+
+    res.end("Please use /bmi url for bmi calc &  use /stress url for stress calc");
+})
 
 app.get('/bmi',(req,res)=>{
   res.sendFile(path.normalize(__dirname+'/BMI.html'));
@@ -12,4 +18,8 @@ app.get('/bmi',(req,res)=>{
 
 app.get('/stress',(req,res)=>{
   res.sendFile(path.normalize(__dirname+'/stress.html'));
-})
+});
+
+app.listen(3000,()=>{
+console.log("server started @ 3000");
+});
